34717cbc0d04f5379de8cc4bd8355f21f365befb
z1kurat <109900803+z1kurat@users.noreply.github.com>
2023-04-02 23:39:24 +0700

    Disable DB

--- GPTbot.py
+++ GPTbot.py
@@ -17,33 +17,35 @@ from aiogram.utils.executor import start_webhook
 
 
 async def on_startup(dispatcher):
-    database.connection()
+    #database.connection()
     await bot.set_webhook(WEBHOOK_URL_BASE + WEBHOOK_URL_PATH, WEBHOOK_SSL_CERT, WEBHOOK_HOST, 40, drop_pending_updates=True)
 
 
 async def on_shutdown(dispatcher):
-    database.disconnect()
+    #database.disconnect()
     await bot.delete_webhook()
 
 
 async def save(user_id, text):
-    await database.execute(f"INSERT INTO messages(telegram_id, text) "
-                           f"VALUES (:telegram_id, :text)", values={'telegram_id': user_id, 'text': text})
+    pass
+    #await database.execute(f"INSERT INTO messages(telegram_id, text) "
+    #                       f"VALUES (:telegram_id, :text)", values={'telegram_id': user_id, 'text': text})
 
 
 async def read(user_id):
-    results = await database.fetch_all('SELECT text '
-                                       'FROM messages '
-                                       'WHERE telegram_id = :telegram_id ',
-                                       values={'telegram_id': user_id})
-    return [next(result.values()) for result in results]
+    return ""
+    # results = await database.fetch_all('SELECT text '
+    #                                    'FROM messages '
+    #                                    'WHERE telegram_id = :telegram_id ',
+    #                                    values={'telegram_id': user_id})
+    # return [next(result.values()) for result in results]
 
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    await save(message.from_user.id, message.text)
-    messages = await read(message.from_user.id)
-    await message.answer(messages)
+    #await save(message.from_user.id, message.text)
+    #messages = await read(message.from_user.id)
+    await message.answer(message.text)
 
 
 if __name__ == '__main__':
