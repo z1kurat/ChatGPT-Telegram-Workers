662bd70315b5fc8c18817f1a81e834b2c058388f
z1kurat <109900803+z1kurat@users.noreply.github.com>
2023-04-02 23:49:47 +0700

    Fix set_webhook

--- GPTbot.py
+++ GPTbot.py
@@ -18,7 +18,7 @@ from aiogram.utils.executor import start_webhook
 
 async def on_startup(dispatcher):
     #database.connection()
-    await bot.set_webhook(WEBHOOK_URL_BASE + WEBHOOK_URL_PATH, WEBHOOK_SSL_CERT, WEBHOOK_HOST, 40, drop_pending_updates=True)
+    await bot.set_webhook(WEBHOOK_HOST, certificate=WEBHOOK_SSL_CERT, drop_pending_updates=True)
 
 
 async def on_shutdown(dispatcher):
