f2370dc3c17c0edb7697243780f804625681520a
z1kurat <109900803+z1kurat@users.noreply.github.com>
2023-04-12 22:38:16 +0700

    Update GPTbot.py

--- GPTbot.py
+++ GPTbot.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
     Filters.Chat_Subscriber.register_filters(dp)
 
     Command.Start.register_handlers(dp)
-    Command.Сhannel_Post_Comment.register_handlers(dp)
     Command.Reset_Context.register_handlers(dp)
     Command.GPT.register_handlers(dp)
+    Command.Сhannel_Post_Comment.register_handlers(dp)
 
     executor.start_polling(dp)
