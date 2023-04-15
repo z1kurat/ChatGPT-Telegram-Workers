7cc06e69fd4c68f97e1aa4e6e9fb7e5df0cb05f9
z1kurat <109900803+z1kurat@users.noreply.github.com>
2023-04-16 01:30:14 +0700

    Update Reset_Context.py

--- Command/Reset_Context.py
+++ Command/Reset_Context.py
@@ -14,6 +14,7 @@ from Command.Command_Name import RESET_COMMAND
 @dp.callback_query_handler(Text(RESET_COMMAND))
 async def cmd_enable_context(callback_query: types.CallbackQuery):
     await DB.del_all_message(callback_query.from_user.id)
+    await DB.set_working(callback_query.from_user.id, False)
     await bot.send_message(callback_query.from_user.id, MESSAGE_RESET_CONTEXT)
     await bot.answer_callback_query(callback_query.id)
 
@@ -21,4 +22,5 @@ async def cmd_enable_context(callback_query: types.CallbackQuery):
 @dp.message_handler(Command(RESET_COMMAND))
 async def cmd_enable_context_command(message: types.Message):
     await DB.del_all_message(message.from_user.id)
+    await DB.set_working(message.from_user.id, False)
     await bot.send_message(message.from_user.id, MESSAGE_RESET_CONTEXT)
