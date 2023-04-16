6ebafcf5391f0ccbf795bfa8327f558dfc933288
z1kurat <109900803+z1kurat@users.noreply.github.com>
2023-04-16 16:55:24 +0700

    Update __init__.py

--- Bot_Command/__init__.py
+++ Bot_Command/__init__.py
@@ -18,7 +18,7 @@ from Filters.Chat_Subscriber import IsSubscriber
 
 
 def register_handler(dp: aiogram.Dispatcher):
-    dp.register_message_handler(Start.cmd_start, Command(START_COMMAND), IsSubscriber())
+    dp.register_message_handler(Start.cmd_start, Command(START_COMMAND))
     dp.register_message_handler(Replay.cmd_replay_command, Command(REPLAY_COMMAND), IsSubscriber())
     dp.register_message_handler(Reset_Context.cmd_enable_context_command, Command(RESET_COMMAND), IsSubscriber())
     dp.register_message_handler(Сhannel_Post_Comment.cmd_comment_gpt, Command(COMMENT_COMMAND), IsSubscriber())
