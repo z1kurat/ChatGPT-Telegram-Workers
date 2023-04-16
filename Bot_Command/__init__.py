401320c1e471ce19250212faf1543fd0c7d83fea
z1kurat <109900803+z1kurat@users.noreply.github.com>
2023-04-16 19:21:07 +0700

    Fix permissons

--- Bot_Command/__init__.py
+++ Bot_Command/__init__.py
@@ -21,7 +21,7 @@ def register_handler(dp: aiogram.Dispatcher):
     dp.register_message_handler(Start.cmd_start, Command(START_COMMAND))
     dp.register_message_handler(Replay.cmd_replay_command, Command(REPLAY_COMMAND), IsSubscriber())
     dp.register_message_handler(Reset_Context.cmd_enable_context_command, Command(RESET_COMMAND), IsSubscriber())
-    dp.register_message_handler(Сhannel_Post_Comment.cmd_comment_gpt, Command(COMMENT_COMMAND), IsSubscriber())
+    dp.register_message_handler(Сhannel_Post_Comment.cmd_comment_gpt, Command(COMMENT_COMMAND))
 
     dp.register_callback_query_handler(Replay.cmd_replay_query, Text(REPLAY_COMMAND))
     dp.register_callback_query_handler(Reset_Context.cmd_enable_context, Text(RESET_COMMAND))
