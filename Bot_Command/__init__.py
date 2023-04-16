7166aab84667e4846fb68f70b84c1e50c2a6685a
z1kurat <109900803+z1kurat@users.noreply.github.com>
2023-04-16 13:48:16 +0700

    Update __init__.py

--- Bot_Command/__init__.py
+++ Bot_Command/__init__.py
@@ -18,10 +18,10 @@ from Filters.Chat_Subscriber import IsSubscriber
 
 
 def register_handler(dp: aiogram.Dispatcher):
-    dp.register_message_handler(Start.cmd_start, filters=(Command(START_COMMAND) & IsSubscriber()))
-    dp.register_message_handler(Replay.cmd_replay_command, filters=(Command(REPLAY_COMMAND) & IsSubscriber()))
-    dp.register_message_handler(Reset_Context.cmd_enable_context_command, filters=(Command(RESET_COMMAND) & IsSubscriber()))
-    dp.register_message_handler(Сhannel_Post_Comment.cmd_comment_gpt, filters=(Command(COMMENT_COMMAND) & IsSubscriber()))
+    dp.register_message_handler(Start.cmd_start, filters=(Command(START_COMMAND), IsSubscriber()))
+    dp.register_message_handler(Replay.cmd_replay_command, filters=(Command(REPLAY_COMMAND), IsSubscriber()))
+    dp.register_message_handler(Reset_Context.cmd_enable_context_command, filters=(Command(RESET_COMMAND), IsSubscriber()))
+    dp.register_message_handler(Сhannel_Post_Comment.cmd_comment_gpt, filters=(Command(COMMENT_COMMAND), IsSubscriber()))
 
     dp.register_callback_query_handler(Replay.cmd_replay_query, filters=(Text(REPLAY_COMMAND)))
     dp.register_callback_query_handler(Reset_Context.cmd_enable_context, filters=(Text(RESET_COMMAND)))
