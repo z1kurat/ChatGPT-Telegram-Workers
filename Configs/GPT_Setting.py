82e110538ed750e1dbf44b55458050a2579648f8
z1kurat <109900803+z1kurat@users.noreply.github.com>
2023-04-16 21:03:53 +0700

    Update GPT_Setting.py

--- Configs/GPT_Setting.py
+++ Configs/GPT_Setting.py
@@ -1,6 +1,6 @@
 DEFAULT_MOD = 'You are a kind and helpful assistant who gives detailed and useful answers'
 
-MODEL = "gpt-3.5-turbo"
+MODEL = "gpt-4"
 TEMPERATURE = 0.7
 MAX_VALUE_COUNT = 1
 CHOICES = 1
