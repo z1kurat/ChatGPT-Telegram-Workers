9cbacb52d12756043e1082ba960b8b742d53dd44
z1kurat <109900803+z1kurat@users.noreply.github.com>
2023-04-16 13:07:09 +0700

    Update __init__.py

--- Bot_Command/__init__.py
+++ Bot_Command/__init__.py
@@ -1,5 +1,5 @@
 from Bot_Command import Start
-from Bot_Command import Reset_Context
 from Bot_Command import Replay
+from Bot_Command import Reset_Context
 from Bot_Command import Сhannel_Post_Comment
 from Bot_Command import GPT
