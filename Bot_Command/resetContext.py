1aeb03bfe7b2c56d898efed3c2530f5d8c24cac6
z1kurat <109900803+z1kurat@users.noreply.github.com>
2023-04-20 00:40:21 +0700

    Style fix

--- Bot_Command/resetContext.py
+++ Bot_Command/resetContext.py
@@ -7,9 +7,7 @@ from Configs.templateResponses import MESSAGE_RESET_CONTEXT
 from DataBase import DB
 
 
-async def reset_conntext(message: types.Message, is_callback=False, callback_id=-1):
-    user_id = message.from_user.id
-
+async def reset_conntext(user_id, is_callback=False, callback_id=-1):
     await DB.delete_user_history(user_id)
     await bot.send_message(user_id, MESSAGE_RESET_CONTEXT)
 
@@ -20,11 +18,12 @@ async def reset_conntext(message: types.Message, is_callback=False, callback_id=
 
 
 async def reset_context_callback(callback_query: types.CallbackQuery):
-    message = callback_query.message
+    user_id = callback_query.from_user.id
     callback_id = callback_query.id
-    await reset_conntext(message, True, callback_id)
+    await reset_conntext(user_id, True, callback_id)
 
 
 async def reset_context_cmd(message: types.Message):
-    await reset_conntext(message)
+    user_id = message.from_user.id
+    await reset_conntext(user_id, False)
 
